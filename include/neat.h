3a3c9df51a087c68bfe1cf8de737dbdcb1584046
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-07-24 09:49:54 +0900

    create base

---
+++ include/neat.h
@@ -0,0 +1,14 @@
+//
+// Created by hungr on 2020/07/24.
+//
+
+#ifndef NEAT_NEAT_H
+#define NEAT_NEAT_H
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+
+
+#endif //NEAT_NEAT_H
---
+++ src/neat.cpp
@@ -0,0 +1,6 @@
+//
+// Created by hungr on 2020/07/24.
+//
+#include "neat.h"
+
+
