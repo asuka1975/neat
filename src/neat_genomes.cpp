6df8d51d9b7c3e891db3d21f7d76907d7854b5c5
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-08-15 15:30:52 +0900

    adapt to genetic-algorithm changes.

--- src/neat_genomes.cpp
+++ src/neat_genomes.cpp
@@ -3,6 +3,6 @@
 //
 #include "neat_genomes.h"
 
-network_information genetic::crossover(const network_information &d1, const network_information &d2) {
+network_information network_information::crossover(const network_information &d1, const network_information &d2) {
     return d1;
 }
