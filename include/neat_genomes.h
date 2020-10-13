8f9a7fcb2c96fc9a63299db6fadc34decc9d5ac4
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-10-14 00:03:06 +0900

    add selection operator (niching)

--- include/neat_genomes.h
+++ include/neat_genomes.h
@@ -6,7 +6,10 @@
 #define NEAT_NEAT_GENOMES_H
 
 #include <functional>
+#include <list>
 #include "network.h"
+#include "ga.h"
+#include "selector.h"
 #include "random_generator.h"
 
 struct node {
@@ -110,5 +113,94 @@ struct network_information : network_information_base {
         return d;
     }
 };
+
+//selection operator (niching)
+template <class... TArgs>
+class species {
+public:
+    explicit species(float dt);
+    std::vector<typename genetic::ga<TArgs...>::individual_t>
+    operator()(const std::vector<typename genetic::ga<TArgs...>::individual_t>& pop, const std::vector<float>& fitness);
+private:
+    float dt;
+    template <class T> class mapply_impl;
+    template <size_t... I>
+    class mapply_impl<std::index_sequence<I...>> {
+        static float mapply(const typename genetic::ga<TArgs...>::individual_t& d1, const typename genetic::ga<TArgs...>::individual_t& d2) {
+            static auto distance = [](std::initializer_list<float> args) -> float {
+                return std::sqrt(std::accumulate(args.begin(), args.end(), 0, [](float a, float b) { return a + b * b; }));
+            };
+            return distance(typename std::tuple_element<I, std::tuple<TArgs...>>::type::distance(std::get<I>(d1), std::get<I>(d2))...);
+        }
+    };
+};
+
+template <class... TArgs>
+inline species<TArgs...>::species(float dt) : dt(dt) {}
+
+template <class... TArgs>
+inline std::vector<typename genetic::ga<TArgs...>::individual_t>
+species<TArgs...>::operator()(const std::vector<typename genetic::ga<TArgs...>::individual_t>& pop, const std::vector<float>& fitness) {
+    std::vector<typename genetic::ga<TArgs...>::individual_t> p(pop.size());
+    std::vector<float> fitness_updated(fitness.size());
+    using index_t = typename std::vector<typename genetic::ga<TArgs...>::individual_t>::size_type;
+    std::vector<std::vector<index_t>> specie(pop.size());
+    for(auto i = 0; i < pop.size(); i++) {
+        float f = 0.0f;
+        for(auto j = 0; j < pop.size(); j++) {
+            if(typename mapply_impl<std::index_sequence_for<TArgs...>>::mapply(pop[i], pop[j]) <= dt) {
+                f += 1.0f;
+                specie[i].push_back(j);
+            }
+        }
+        fitness_updated[i] = fitness[i] / f;
+    }
+    std::vector<index_t> idx(pop.size()); std::iota(idx.begin(), idx.end());
+    std::sort(idx.begin(), idx.end(), [&fitness_updated](auto i, auto j) {
+        return fitness_updated[i] < fitness_updated[j];
+    });
+    std::for_each(fitness_updated.begin(), fitness_updated.end(), [m=*std::min_element(fitness_updated.begin(), fitness_updated.end())](auto& x) {
+        x -= m;
+    });
+    if(float s = s=std::accumulate(fitness_updated.begin(), fitness_updated.end(), 0.0f); std::abs(s) < std::numeric_limits<float>::epsilon()) {
+        std::for_each(fitness_updated.begin(), fitness_updated.end(), [s=1.0f/p.size()] (auto& x) {
+            x = s;
+        });
+    } else {
+        std::for_each(fitness_updated.begin(), fitness_updated.end(), [s] (auto& x) {
+            x /= s;
+        });
+    }
+
+    for(auto i = 0; i < pop.size(); i++) {
+        auto r = random_generator::random<float>();
+        float range = 0;
+        index_t j;
+        for(j = 0; j < pop.size(); j++) {
+            if(range <= r && r < range + fitness_updated[j]) {
+                break;
+            }
+            range += fitness_updated[j];
+        }
+        if(specie[j].empty()) p[i] = pop[j];
+        else {
+            float range_ = 0;
+            index_t k;
+            r = random_generator::random_uniform<float>(0, std::accumulate(specie[j].begin(), specie[j].end(), 0.0f, [&fitness_updated](auto a, auto b) {
+                return a + fitness_updated[b];
+            }));
+            for(index_t l = 0, k = specie[j][l]; l < specie[j].size(); k = specie[j][++l]) {
+                if(range_ <= r && r < range_ + fitness_updated[k]) {
+                    break;
+                }
+                range_ += fitness_updated[k];
+            }
+            p[i] = genetic::crossover(pop[j], pop[k]);
+        }
+    }
+    return p;
+}
+
+
 
 #endif //NEAT_NEAT_GENOMES_H
